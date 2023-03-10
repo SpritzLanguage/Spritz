b1c351091deedf428d9a0655d67e9b6d99154989
Wolfsurge <iantopettitt123@gmail.com>
2023-03-10 18:11:27 +0000

    More String tasks

--- src/main/kotlin/spritz/value/string/StringValue.kt
+++ src/main/kotlin/spritz/value/string/StringValue.kt
@@ -2,13 +2,14 @@ package spritz.value.string
 
 import spritz.Spritz
 import spritz.api.annotations.Excluded
+import spritz.api.annotations.Identifier
 import spritz.error.Error
 import spritz.interpreter.context.Context
 import spritz.lexer.token.Token
 import spritz.value.PrimitiveReferenceValue
 import spritz.value.Value
 import spritz.value.bool.BoolValue
-import spritz.value.number.IntValue
+import spritz.value.list.ListValue
 
 /**
  * @author surge
@@ -61,7 +62,8 @@ class StringValue(val value: String) : Value("string") {
 
     class Companion(@Excluded val string: StringValue) {
 
-        fun get(index: Int): String {
+        @Identifier("char_at")
+        fun charAt(index: Int): String {
             return this.string.value[index].toString()
         }
 
@@ -69,8 +71,45 @@ class StringValue(val value: String) : Value("string") {
             return this.string.value.substring(start, end)
         }
 
-        fun size(): Int {
+        fun length(): Int {
             return this.string.value.length
+        }
+
+        @Identifier("is_empty")
+        fun isEmpty(): Boolean {
+            return this.string.value.isEmpty()
+        }
+
+        @Identifier("starts_with")
+        fun startsWith(prefix: String): Boolean {
+            return this.string.value.startsWith(prefix)
+        }
+
+        @Identifier("ends_with")
+        fun endsWith(suffix: String): Boolean {
+            return this.string.value.endsWith(suffix)
+        }
+
+        @Identifier("index_of")
+        fun indexOf(string: String): Int {
+            return this.string.value.indexOf(string)
+        }
+
+        @Identifier("last_index_of")
+        fun lastIndexOf(string: String): Int {
+            return this.string.value.lastIndexOf(string)
+        }
+
+        fun replace(var1: String, var2: String): String {
+            return this.string.value.replace(var1, var2)
+        }
+
+        fun split(deliminator: String): ListValue {
+            return ListValue(this.string.value.split(deliminator).map { StringValue(it) }.toMutableList())
+        }
+
+        fun trim(): String {
+            return this.string.value.trim()
         }
 
     }
